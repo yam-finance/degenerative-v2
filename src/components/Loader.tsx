5d94eb0d3a8c4ea9d00fe0d6247ea1460ef496ae
byterose <>
2021-05-07 17:19:03 +0200

    Add css name changes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -9,10 +9,10 @@ export const Loader = () => {
   return (
     <div>
       <span>
-        <svg className="circleSvg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <svg className="circle-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <circle className="circle" cx={12} cy={12} r={10} stroke="currentColor" strokeWidth={4} />
           <path
-            className="circleSvgPath"
+            className="circle-svg-path"
             fill="currentColor"
             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
           />
--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -7,17 +7,17 @@ import React from 'react';
 
 export const Tooltip = (props: any) => {
   return (
-    <div className={`svgContainer ${props.position == 'top' ? 'svgPosTop' : 'svgPosBottom'}`}>
-      <div className="svgText">
+    <div className={`svg-container ${props.position == 'top' ? 'svg-pos-top' : 'svg-pos-bottom'}`}>
+      <div className="svg-text">
         {props.tooltipText}
         <svg
-          className={`svgTriangle ${props.tooltipStyle == 'center' ? 'positionCenter' : props.tooltipStyle == 'right' ? 'positionRight' : 'positionLeft'}`}
+          className={`svg-triangle ${props.tooltipStyle == 'center' ? 'position-center' : props.tooltipStyle == 'right' ? 'position-right' : 'position-left'}`}
           x="0px"
           y="0px"
           viewBox="0 0 255 255"
           xmlSpace="preserve"
         >
-          <polygon className="tooltipPolygon" points="0,0 127.5,127.5 255,0" />
+          <polygon className="tooltip-polygon" points="0,0 127.5,127.5 255,0" />
         </svg>
       </div>
     </div >
