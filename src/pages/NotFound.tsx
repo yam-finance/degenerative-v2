7b3964396460a0b3608cf25cf3747b4cbb8a1890
Blokku-chan <blokku.chan@gmail.com>
2021-05-15 11:28:29 +0100

    Tweaked top padding

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,7 +7,7 @@ export const NotFound = () => {
     <Page>
       <Navbar />
       <MainDisplay>
-        <div className="width-full height-full padding-4 padding-top-40 flex-column-centered">
+        <div className="width-full height-full padding-4 padding-top-32 flex-column-centered">
           <img className="width-full max-width-2xl" src="/src/assets/404.png"></img>
           <h1 className="text-align-center">Fren, you lost? Head back home</h1>
           <Link to="/" className="button">Head Home</Link>
