8458c3dd41e86729430c041e1f53794d687ac758
sali <saadali93@gmail.com>
2021-06-08 16:54:05 -0500

    Change up charts

--- src/utils/ReferencePrices.ts
+++ src/utils/ReferencePrices.ts
@@ -13,12 +13,11 @@ export const getReferencePriceHistory = async (type: string, chainId: number) =>
 
     return res.data.map(({ timestamp, price }: { timestamp: number; price: number }) => {
       const dateString = getDateString(fromUnixTime(timestamp));
-      const usdPriceCollateral = (collateralUsd.get(dateString) ?? 1) / 10 ** 9;
-      const scaledPrice = price / 1000; // Numbers don't work without dividing by 1000. Not sure why.
+      const scaledPrice = price / 10 ** 12; // uGAS represents 1,000,000 gas
 
       return {
         timestamp: dateString,
-        price: roundDecimals(scaledPrice * usdPriceCollateral, 2),
+        price: roundDecimals(scaledPrice, 6),
       };
     });
   };
