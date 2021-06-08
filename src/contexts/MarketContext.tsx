964b6669baad8a6f120fdee3e37e6dec5e9c9bb2
byterose <>
2021-06-08 23:57:38 +0200

    Update

--- src/contexts/MarketContext.tsx
+++ src/contexts/MarketContext.tsx
@@ -102,13 +102,7 @@ export const MarketProvider: React.FC = ({ children }) => {
 
             // Grab APRs from API
             //const apr = roundDecimals(Math.random() * 100, 2); // TODO get actual APR
-            // tvl: collateral amount
-            // totalSupply: tokenCount
-            // priceUsd
-            // marketCap
-            // console.log(totalSupply.toString())
-            // const apr = (await getMiningRewards(synth, tvlUsd, totalSupply, priceUsd, marketCap)) ?? 0;
-            const tokenCount = BigNumber.from(utils.formatUnits(totalSupply, paired.decimals))
+            const tokenCount = BigNumber.from(Number(utils.formatUnits(totalSupply, paired.decimals)))
             const apr = (await getMiningRewards(name, synth, priceUsd, 1.5, tokenCount) ?? 0);
 
             data[name] = {
