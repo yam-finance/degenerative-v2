ecdbdbd959f93b3de5cad4df979385596a69466b
Ramy Melo <ramysmelo@gmail.com>
2021-07-21 19:35:42 -0400

    Added aria-label to breadcrumbs and modified test to look for the breadcrumbs.

--- tests/Explore.spec.tsx
+++ tests/Explore.spec.tsx
@@ -1,35 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import { Explore } from '@/pages/Explore';
 import ContextProviders from '@/contexts';
 import { MemoryRouter as Router, Route, Switch } from 'react-router-dom';
 
+function renderWithTestContext(currentPath = '/explore', routePath = '/explore') {
+  return render(
+    <Router initialEntries={[{ pathname: currentPath }]}>
+      <ContextProviders>
+        <Switch>
+          <Route exact strict path={routePath} component={Explore} />
+        </Switch>
+      </ContextProviders>
+    </Router>
+  );
+}
+
 describe('The Explore Page', () => {
-  const explorePathName = '/explore';
-  it('should render on correct pathname', () => {
-    const pathname = explorePathName;
-    const { getByText } = render(
-      <Router initialEntries={[{ pathname }]}>
-        <ContextProviders>
-          <Switch>
-            <Route exact strict path={explorePathName} component={Explore} />
-          </Switch>
-        </ContextProviders>
-      </Router>
-    );
-    expect(getByText('Yam Synths')).toBeInTheDocument();
-  });
-  it('should not render on incorrect pathname', () => {
-    const pathname = '/details';
-    const { queryByText } = render(
-      <Router initialEntries={[{ pathname }]}>
-        <ContextProviders>
-          <Switch>
-            <Route exact strict path={explorePathName} component={Explore} />
-          </Switch>
-        </ContextProviders>
-      </Router>
-    );
-    expect(queryByText('Yam Synths')).not.toBeInTheDocument();
+  it('should have breadcrumbs that point to the current location', () => {
+    renderWithTestContext();
+    // ideally we should query for a test-id attribute.
+    const breadcrumbsElement = screen.getByRole('navigation', { name: 'breadcrumbs' });
+    const exploreLinkElement = within(breadcrumbsElement).getByText('Explore').parentElement;
+    expect(exploreLinkElement).toHaveAttribute('aria-current', 'page');
   });
 });
