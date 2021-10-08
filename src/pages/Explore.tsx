6c6f287a0f43ee69a73ddc9306c70702110c0c77
jason <kwon_jason@yahoo.com>
2021-10-09 01:24:16 +1000

    add legacy

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -15,6 +15,15 @@ interface ISynthGroupData {
   numSynths: number;
   image: string;
 }
+interface ILegacyGroupData {
+  apr: number;
+  totalLiquidity: number;
+  totalMarketCap: number;
+  totalTvl: number;
+  totalVolume24h: number;
+  numSynths: number;
+  image: string;
+}
 
 export const Explore = () => {
   const { loading, synthMetadata, synthMarketData } = useContext(MarketContext);
@@ -23,18 +32,22 @@ export const Explore = () => {
   const [searchTerm, setSearchTerm] = useState(query.get('search') ?? '');
   const [sidebarData, setSidebarData] = useState<string>('');
   const [synthGroupData, setSynthGroupData] = useState<Record<string, ISynthGroupData>>({});
+  const [legacyGroupData, setLegacyGroupData] = useState<Record<string, ILegacyGroupData>>({});
 
   useEffect(() => {
     const AggregateSynthGroupData = () => {
       const aggregateData: Record<string, ISynthGroupData> = {};
+      const aggregateLegacyData: Record<string, ILegacyGroupData> = {};
+     
 
-      console.log("meta = ", synthMetadata);
+      console.log("synthMarketData = ", synthMarketData);
       Object.entries(synthMetadata)
         .filter(([synthName, synthInfo]) => synthName.toUpperCase().includes(searchTerm.toUpperCase()))
+        .filter(([synthName, synthInfo])=>!synthInfo.legacy||synthInfo.legacy==false)
         .forEach(([synthName, synthInfo]) => {
           // Loop through all synths in group, find cumulative data for display
           const { group } = synthInfo;
-          console.log("group = ", group);
+          console.log("synthInfo = ", synthInfo);
           console.log("namer = ", synthName)
           console.log("aggregateDataBefore = ", aggregateData);
           try {
@@ -75,11 +88,60 @@ export const Explore = () => {
             };
           }
         });
-        console.log("aggregateData = ", aggregateData);
+        Object.entries(synthMetadata)
+        .filter(([synthName, synthInfo]) => synthName.toUpperCase().includes(searchTerm.toUpperCase()))
+        .filter(([synthName, synthInfo])=>synthInfo.legacy&&synthInfo.legacy==true)
+        .forEach(([synthName, synthInfo]) => {
+          // Loop through all synths in group, find cumulative data for display
+          const { group } = synthInfo;
+        
+         
+          try {
+            const marketData = synthMarketData[synthName];
+
+            // Data brought from last loop iteration
+            const currentData = aggregateLegacyData[group] ?? {
+              apr: 0,
+              aprMax: 0,
+              totalLiquidity: 0,
+              totalMarketCap: 0,
+              totalTvl: 0,
+              totalVolume24h: 0,
+              numSynths: 0,
+              image: '',
+            };
+
+            const apr = marketData.apr >= currentData.apr ? marketData.apr : currentData.apr;
+
+            aggregateLegacyData[group] = {
+              apr: apr,
+              totalLiquidity: currentData.totalLiquidity + marketData.liquidity,
+              totalMarketCap: currentData.totalMarketCap + marketData.marketCap,
+              totalTvl: currentData.totalTvl + marketData.tvl,
+              totalVolume24h: currentData.totalVolume24h + marketData.volume24h,
+              numSynths: currentData.numSynths + 1,
+              image: `/images/${SynthGroups[group].image}.png`,
+            };
+          } catch (err) {
+            aggregateLegacyData[group] = {
+              apr: 0,
+              totalLiquidity: 0,
+              totalMarketCap: 0,
+              totalTvl: 0,
+              totalVolume24h: 0,
+              numSynths: 1,
+              image: 'src/assets/images/Box-01.png',
+            };
+          }
+        });
+        console.log("aggregateData = ", aggregateLegacyData);
       setSynthGroupData(aggregateData);
+      setLegacyGroupData(aggregateLegacyData);
     };
+   console.log("legacy groupdata = ", legacyGroupData);
 
     if (!isEmpty(synthMetadata) && !isEmpty(synthMarketData)) AggregateSynthGroupData();
+   
   }, [synthMarketData, searchTerm]);
 
   const SynthGroupBlock: React.FC<{ group: string }> = ({ group }) => {
@@ -111,7 +173,35 @@ export const Explore = () => {
             <img src={image} loading="lazy" className="margin-1 radius-full" />
           </div>
           <div>
-            <div className="margin-right-1 text-color-4">{group=="uSTONKS"?"LEGACY":group}</div>
+            <div className="margin-right-1 text-color-4">{group}</div>
+            <div className="text-xs opacity-50">{description}</div>
+          </div>
+        </div>
+        <div></div>
+        <div className="expand portrait-padding-y-2">
+          <div className="text-color-4">{`${apr}%`}</div>
+        </div>
+        <div className="expand portrait-padding-y-2">
+          <div className="text-color-4">${formatForDisplay(totalLiquidity)}</div>
+        </div>
+        <div className="expand portrait-padding-y-2">
+          <div className="text-color-4">${formatForDisplay(totalMarketCap)}</div>
+        </div>
+      </TableRow>
+    );
+  };
+  const LegacyGroupRow: React.FC<{ group: string }> = ({ group }) => {
+    const { apr, totalLiquidity, totalMarketCap, image } = legacyGroupData[group];
+    const { description } = SynthGroups[group];
+
+    return (
+      <TableRow to={`/explore/${group}`} onMouseEnter={() => setSidebarData(group)}>
+        <div className="flex-align-center portrait-width-full width-1-2">
+          <div className="width-10 height-10 flex-align-center flex-justify-center radius-full background-white-50 margin-right-2">
+            <img src={image} loading="lazy" className="margin-1 radius-full" />
+          </div>
+          <div>
+            <div className="margin-right-1 text-color-4">{group}</div>
             <div className="text-xs opacity-50">{description}</div>
           </div>
         </div>
@@ -182,13 +272,18 @@ export const Explore = () => {
           <>
             <div className="grid-3-columns margin-x-8 margin-top-4">
               {Object.keys(synthGroupData).map((group, index) => {
-                if(group!="uSTONKS")
+               
                 return <SynthGroupBlock group={group} key={index} />;
               })}
             </div>
             <Table headers={['Synth', 'APR', 'Liquidity', 'Market Cap']} headerClass={['width-1-2', '', '', '']}>
               {Object.keys(synthGroupData).map((group, index) => {
                 return <SynthGroupRow group={group} key={index} />;
+              })}
+            </Table>
+            <Table headers={['Legacy', '', '', '']} headerClass={['width-1-2', '', '', '']}>
+              {Object.keys(legacyGroupData).map((group, index) => {
+                return <LegacyGroupRow group={group} key={index} />;
               })}
             </Table>
           </>
